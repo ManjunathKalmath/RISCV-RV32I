d19b6b488f33e8b564313948e71ee26fb36054c3
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-10-30 18:23:04 +0530

    Update Register_File.v

--- RTL/Register_File.v
+++ RTL/Register_File.v
@@ -1,1 +1,15 @@
-//Need to Add Regiter File Code
+//RISCV Register File
+module Register_File(Read_Reg1,Read_Reg2,Read_Data1,Read_data2,Clock,Write_Reg,Write_Data,Write);
+ input [5:0] Read_Reg1,Read_Reg2,Write_Reg;
+ input [31:0] Write_Data;
+ input Write,clock;//here write is the Control to Register File
+ reg [31:0] Reg_File [31:0];//32 registres each of 32 bit long
+ 
+ assign Read_Data1 = Reg_File[Read_reg1];
+ assign Read_Data2 = Reg_File[Read_reg2];
+ always @(posedge clock)
+  begin
+  if(Write)
+   Reg_File[Write_reg] <= Write_Data;
+  end
+endmodule
