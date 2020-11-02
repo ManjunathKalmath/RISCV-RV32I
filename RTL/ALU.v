1dbaf9847ee1504b5f63c1dd9bdb41fe31b4eaa9
sidhantp1906 <60102705+sidhantp1906@users.noreply.github.com>
2020-11-02 11:36:09 +0530

    Update ALU.v

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -1,1 +1,20 @@
 //Need to add ALU code
+module RISCV_ALU(ALU_Ctrl,A,B.ALU_Out);
+ input [31:0] A,B;
+ input ALU_Ctrl;
+ output reg [31:0] ALU_Out;
+ outut zero;
+ if(ALU_Out == 0)
+  zero = ALU_Out;
+ always @(*)
+  begin
+   case(ALU_Ctrl)
+    0: ALU_Out <= A + B;
+    1: ALU_Out <= A - B;
+    2: ALU_Out <= A | B;
+    3: ALU_Out <= A & B;
+    4: ALU_Out <= A < B ? 1 : 0;
+    default ALU_Out <= 0;
+   endcase
+  end
+endmodule
