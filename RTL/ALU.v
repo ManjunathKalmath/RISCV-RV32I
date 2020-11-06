b798da64c8cc57b313d119fe1226a4241c2cf179
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-06 17:47:37 +0530

    Update ALU.v

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -1,6 +1,6 @@
 module RISCV_ALU(ALU_Ctrl,A,B,ALU_Out);
  input [31:0] A,B;
- input [3:0] ALU_Ctrl;
+ input [4:0] ALU_Ctrl;
  output reg [31:0] ALU_Out;
  outut zero;
  if(ALU_Out == 0)
@@ -8,13 +8,26 @@ module RISCV_ALU(ALU_Ctrl,A,B,ALU_Out);
  always @(*)
   begin
    case(ALU_Ctrl)
-    2 : ALU_Out <= A + B;
-    6 : ALU_Out <= A - B;
-    1 : ALU_Out <= A | B;
-    0 : ALU_Out <= A & B;
-    7 : ALU_Out <= A < B ? 1 : 0;
-   12 : ALU_Out <= ~ (A | B);
-    default ALU_Out <= 0;
+    1 : ALU_Out <= A + B; //ADD
+    2 : ALU_Out <= A + Imm; //ADDI
+    3 : ALU_Out <= A | B; //OR
+    4 : ALU_Out <= A | Imm; //ORI;
+    5 : ALU_Out <= A ^ B; //XOR
+    6 : ALU_Out <= A ^ Imm; //XORI;
+    7 : ALU_Out <= A & B; //AND
+    8 : ALU_Out <= A & Imm; //ANDI;
+    9 : ALU_Out <= A - B; //SUB
+   10 : ALU_Out <= A < B ? 1 : 0; //SLT
+   11 : ALU_Out <=  //SLTI
+   12 : ALU_Out <= //SLTU
+   13 : ALU_Out <= //SLTIU
+   14 : ALU_Out <= //SLLI
+   15 : ALU_Out <= //SRLI
+   16 : ALU_Out <= //SRAI
+   17 : ALU_Out <= //SLL
+   18 : ALU_Out <= //SRL
+   19 : ALU_Out <= //SRA
+   default ALU_Out <= 0;
    endcase
   end
 endmodule
