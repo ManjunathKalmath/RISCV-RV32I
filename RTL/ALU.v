71b2d66b2d59648ffd9e0f9c5e43de3bcaa8fcb0
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-06 16:17:59 +0530

    Update ALU.v

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -1,18 +1,19 @@
-module RISCV_ALU(ALU_Ctrl,A,B,ALU_Out);
+module RISCV_ALU(ALU_Ctl,A,B,ALU_Out);
  input [31:0] A,B;
- input [3:0] ALU_Ctrl;
+ input [3:0] ALU_Ctl;
  output reg [31:0] ALU_Out;
  outut zero;
  if(ALU_Out == 0)
   zero = ALU_Out;
  always @(*)
   begin
-   case(ALU_Ctrl)
-    0: ALU_Out <= A + B;
-    1: ALU_Out <= A - B;
-    2: ALU_Out <= A | B;
-    3: ALU_Out <= A & B;
-    4: ALU_Out <= A < B ? 1 : 0;
+   case(ALU_Ctl)
+    2 : ALU_Out <= A + B;
+    6 : ALU_Out <= A - B;
+    1 : ALU_Out <= A | B;
+    0 : ALU_Out <= A & B;
+    7 : ALU_Out <= A < B ? 1 : 0;
+   12 : ALU_Out <= ~ (A | B);
     default ALU_Out <= 0;
    endcase
   end
