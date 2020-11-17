bd1be605a3e7f58d81eb3389ce2778604c2d02e4
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-17 16:39:35 +0530

    Update Datapath.v

--- RTL/Datapath.v
+++ RTL/Datapath.v
@@ -1,11 +1,11 @@
 module RV32IM_Datapath(clock);
   input clock;
-  input Write,Bsel;
+  input Write,Bsel,Immsel;
   
   reg [31:0] PC,IR,ALU_Out;
   reg [31:0] Memory [0:1023];
   
-  wire [31:0] A,B;
+  wire [31:0] A,B,Imm;
   wire zero;
   wire [4:0] ALU_Ctrl;
   
@@ -16,6 +16,11 @@ module RV32IM_Datapath(clock);
   assign rs1 = IR[19:15];
   assign rs2 = IR[24:20];
   
+  if(Immsel == I)
+    begin
+      assign Imm = {{20{IR[31]}}, IR[30:20]}; //sign extension of IR[31] to upper Imm value
+    end
+        
   Fetch = Memory[PC];
   PC <= PC + 4;
   Register_File RF_RV(rs1,rs2,A,B,Clock,Write_Reg,Write_Data,Write=0);
