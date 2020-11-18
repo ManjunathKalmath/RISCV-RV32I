7a5b98efc19fbb518d14ee0ff321c436ce9f9dbf
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-18 09:07:31 +0530

    Create Branch_Comparator.v

---
+++ RTL/Branch_Comparator.v
@@ -0,0 +1,16 @@
+module Branch_Comparator(Read_Data1,Read_Data2,BrUn,BrEq,BrLT,BrGE);
+ input [31:0] Read_Data1,Read_Data2;
+ output BrUn,BrEq,BrLT,BrGE;
+ 
+ begin
+ if(Read_Data1 == Read_Data2)
+  BrEq = 1;
+ else if(Read_Data1 < Read_Data2)
+ BrLT = 1;
+ else if(!(Read_Data1 < Read_Data2))
+ BrGE = 1;
+ else
+ $display("Error");
+ end
+ 
+endmodule
