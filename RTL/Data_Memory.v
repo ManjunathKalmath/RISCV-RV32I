814a63170d45794b8156f302b9f5e9c3c6cb2311
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-18 08:12:34 +0530

    Update Data_Memory.v

--- RTL/Data_Memory.v
+++ RTL/Data_Memory.v
@@ -1,10 +1,15 @@
-module Data_Memory(Address,MemRW,clock);
-input Address,MemRW;
-
-reg [31:0] Memory [0:63];
-
-always @(posedge clock)
-begin
-
-end
+module Data_Memory(Address,Mem_write_data,MemRW,DataR,clock);
+  input clock,MemRW;
+  input [31:0] Address,Mem_write_data;  
+  output [31:0] DataR;
+  
+  reg [31:0] Memory [0:1023];//test it while writing test bench and make changes
+  
+  always @(posedge clock)
+    begin
+      if(MemRW == 1)
+        Memory[Address] <= Mem_write_data;
+      else
+        DataR <= Memory[Address];
+    end
 endmodule
