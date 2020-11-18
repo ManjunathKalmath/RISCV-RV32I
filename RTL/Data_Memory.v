aa486d6dc1cad264bd8000a7a23c40c446a11185
Manjunath Kalmath <48850794+ManjunathKalmath@users.noreply.github.com>
2020-11-18 08:32:58 +0530

    Update Data_Memory.v

--- RTL/Data_Memory.v
+++ RTL/Data_Memory.v
@@ -1,7 +1,7 @@
-module Data_Memory(Address,Mem_write_data,MemRW,DataR,clock);
+module Data_Memory(Address,Mem_write_data,MemRW,Mem_read_data,clock);
   input clock,MemRW;
   input [31:0] Address,Mem_write_data;  
-  output [31:0] DataR;
+  output [31:0] Mem_read_data;
   
   reg [31:0] Memory [0:1023];//test it while writing test bench and make changes
   
@@ -10,6 +10,6 @@ module Data_Memory(Address,Mem_write_data,MemRW,DataR,clock);
       if(MemRW == 1)
         Memory[Address] <= Mem_write_data;
       else
-        DataR <= Memory[Address];
+        Mem_read_data <= Memory[Address];
     end
 endmodule
